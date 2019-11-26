ef19de309fcd0d1cc283cc2308c1bf08147aeea9
lucapu88 <caputoluca88@gmail.com>
2019-11-26 16:35:23 +0100

    aggiunto in particolare anche l'esercizio di ieri

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 
+//al click su un elemento in lista il sottomenù appare ogni volta che clicco su un elemento, facendo scoparire quello cliccato in precedenza
 $(document).ready(function() {
-  $('li').click(function() {  //al click su un elemento in lista
+  $('li').click(function() {
     if ($(this).children('.drop-down-menu').length > 1) {
       $('.drop-down-menu').slideUp();
-    } else {           //il sottomenù appare ogni volta che clicco su un elemento, facendo scoparire quello cliccato in precedenza
+    } else {
       $('.drop-down-menu').slideUp();
       $(this).children('.drop-down-menu').slideToggle();
     }
+  });
+  $('.header-right > a').click(function() { //cliccando sull'icona dell'hamburger faccio apparire il menu
+    $('.hamburger-menu').addClass('active');
+  });
+  $('.close').click(function() { //cliccando sull'icona della 'x' faccio scomparire il menu
+    $('.hamburger-menu').removeClass('active');
   });
 });

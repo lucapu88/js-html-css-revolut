c4255bda9d08f105b976e0d3bfdbc389930e7403
lucapu88 <caputoluca88@gmail.com>
2019-11-26 15:58:58 +0100

    modifiche e commenti

--- main.js
+++ main.js
@@ -1,16 +1,9 @@
-// $(document).ready(function() {
-//   $('.header-right > a').click(function() { //cliccando sull'icona dell'hamburger faccio apparire il menu
-//     $('.hamburger-menu').addClass('active');
-//   });
-//   $('.close').click(function() { //cliccando sull'icona della 'x' faccio scomparire il menu
-//     $('.hamburger-menu').removeClass('active');
-//   });
-// })
+
 $(document).ready(function() {
-  $('li').click(function() {
+  $('li').click(function() {  //al click su un elemento in lista
     if ($(this).children('.drop-down-menu').length > 1) {
       $('.drop-down-menu').slideUp();
-    } else {
+    } else {           //il sottomenù appare ogni volta che clicco su un elemento, facendo scoparire quello cliccato in precedenza
       $('.drop-down-menu').slideUp();
       $(this).children('.drop-down-menu').slideToggle();
     }

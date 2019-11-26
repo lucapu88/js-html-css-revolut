99f7ef31991d458b58695a7456d625c8a464856e
lucapu88 <caputoluca88@gmail.com>
2019-11-26 15:23:09 +0100

    stilizzazione darra

---
+++ main.js
@@ -0,0 +1,8 @@
+// $(document).ready(function() {
+//   $('.header-right > a').click(function() { //cliccando sull'icona dell'hamburger faccio apparire il menu
+//     $('.hamburger-menu').addClass('active');
+//   });
+//   $('.close').click(function() { //cliccando sull'icona della 'x' faccio scomparire il menu
+//     $('.hamburger-menu').removeClass('active');
+//   });
+// })

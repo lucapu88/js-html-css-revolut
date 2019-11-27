eda16b0bf0291aaa21e35e32cd00f2af91d4953e
lucapu88 <caputoluca88@gmail.com>
2019-11-27 10:41:57 +0100

    correzione in classe: meglio utilizzare ".is(':visible')" invece che ".length > 1"

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 
 //al click su un elemento in lista, il sottomenù appare ogni volta che clicco su un elemento facendo scoparire quello cliccato in precedenza
 $(document).ready(function() {
-  $('li').click(function() {
-    if ($(this).children('.drop-down-menu').length > 1) {
+  $('li').click(function() { // se l'elemento è visibile il drop-down-menu sale
+    if ($(this).children('.drop-down-menu').is(':visible')) {
       $('.drop-down-menu').slideUp();
     } else {
-      $('.drop-down-menu').slideUp();
+      $('.drop-down-menu').slideUp(); //al click su un altro drop-down-menu, scende quello cliccato e sale il dropdown menu  cliccato in precedenza
       $(this).children('.drop-down-menu').slideToggle();
     }
   });
+
   //Se viene visualizzato al di sotto dei 1000px
   $('.header-right > a').click(function() { //cliccando sull'icona dell'hamburger faccio apparire il menu
     $('.hamburger-menu').addClass('active');
